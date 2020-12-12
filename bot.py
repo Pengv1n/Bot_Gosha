844ccd5943c270c0c8319b69a8fb457ac6172a4c
georg <georgiy_gosha_09@mai;.ru>
2020-12-13 01:44:18 +0300

    //

--- bot.py
+++ bot.py
@@ -1,16 +1,16 @@
-import pyowm
+import pyowm #API позволяющее получить погоду
 import telebot
 import random
-from telebot import types
-from bs4 import BeautifulSoup
-import requests as req
-import re
+from telebot import types #для добавления кнопок
+from bs4 import BeautifulSoup #позволяет легко парсить сайты
+import requests as req #позволяет получить код HTML по ссылке сайта
+import re # регулярные выражения
 import os
-os.system('chcp 65001')
+os.system('chcp 65001') #была проблема с кодировкой при парсинге, единственное решение которое помогло энкодить в UTF-8
 
 #30 цитат
-quote=[]
-author=[]
+quote=[] #список цитат
+author=[]#список авторов цитат
 def parsing_quotes():
     url='https://resheto.net/raznosti/143-pozitivnye-tsitaty-na-kazhdyj-den'
     response = req.get(url)
@@ -26,8 +26,8 @@ def parsing_quotes():
 parsing_quotes()
 
 #114 стихотворений
-stixs=[]
-names=[]
+stixs=[]#список стихотворений
+names=[]#cписок авторов и названий стихотворений
 def parsing_stixs():
     url = 'https://poemata.ru/rating/russian/'
     response = req.get(url)
@@ -47,22 +47,22 @@ def parsing_stixs():
             continue
 parsing_stixs()
 
-owm =pyowm.OWM('b3377cb02dafa025857e3fdc8b022828')
+owm =pyowm.OWM('b3377cb02dafa025857e3fdc8b022828') #токен, необходимый для получения погоды
 
-token='1313378892:AAHD3klFSAvNdC9508OWTY6aZzCRq74zNQw'
+token='1313378892:AAHD3klFSAvNdC9508OWTY6aZzCRq74zNQw' #токен бота
 bot = telebot.TeleBot(token)
 
-@bot.message_handler(commands=['start','help'])
+@bot.message_handler(commands=['start','help']) #быстрые команды, будут выполняться при вводе /start или /help
 def start(message):
     bot.send_message(message.from_user.id, 'Привет!\nВот что ты можешь получить от меня:\n---Погоду\n---Цитату\n---Стихотворение\nВведи что-нибудь')
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(content_types=['text']) #функция выполняющаяся при каждом вводе текстового сообщения
 def content(message):
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
     itembtn1 = types.KeyboardButton('Узнать погоду')
     itembtn2 = types.KeyboardButton('Получить цитату')
     itembtn3 = types.KeyboardButton('Хочу стихотворение')
-    markup.add(itembtn1,itembtn2,itembtn3)
+    markup.add(itembtn1,itembtn2,itembtn3) #добавление кнопок
     print(message.from_user)
     bot.send_message(message.from_user.id,'Чем могу помочь?', reply_markup=markup)
     bot.register_next_step_handler(message, get_city)
@@ -70,12 +70,12 @@ def content(message):
     bot.register_next_step_handler(message, get_stix)
 
 
-def get_city(message):
+def get_city(message): # приглашает ввести желаемый город
     if message.text=='Узнать погоду':
         bot.send_message(message.from_user.id, "Введи город, для которого хочешь узнать погоду")
         bot.register_next_step_handler(message, get_temperature)
 
-def get_temperature(message):
+def get_temperature(message): #выводит температуру
     city=message.text
     try:
         wo = owm.weather_at_place(city).get_weather()
@@ -88,16 +88,16 @@ def get_temperature(message):
     except:
         bot.send_message(message.from_user.id,"Такого города не существует")
 
-def get_stix(message):
+def get_stix(message):  #достает из списка случайное стихотворение
     if message.text=='Хочу стихотворение':
         t = random.randint(0, len(stixs))
         bot.send_message(message.from_user.id, '*' + names[t] + '*\n\n'+stixs[t],
                          parse_mode="Markdown")
 
-def get_quote(message):
+def get_quote(message): #достает из списка цитату
     if message.text=='Получить цитату':
         t=random.randint(0, len(quote))
         bot.send_message(message.from_user.id,str(quote[t])+'\n--'+'*'+str(author[t])+'*',parse_mode= "Markdown")
 
 
-bot.polling(none_stop=True, interval=0)
+bot.polling(none_stop=True, interval=0) #необходимо для непрерыной работы бота
