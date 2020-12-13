155cd5448347102ef6b422791718c4230a545d4f
georg <georgiy_gosha_09@mai;.ru>
2020-12-13 18:35:06 +0300

    final

--- bot.py
+++ bot.py
@@ -54,7 +54,54 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start','help']) #быстрые команды, будут выполняться при вводе /start или /help
 def start(message):
-    bot.send_message(message.from_user.id, 'Привет!\nВот что ты можешь получить от меня:\n---Погоду\n---Цитату\n---Стихотворение\nВведи что-нибудь')
+    bot.send_message(message.from_user.id, 'Привет!\nВот что ты можешь получить от меня:'
+                                           '\n---Погоду\n---Цитату\n---Стихотворение'
+                                           '\nТакже ты можешь добавить города для быстрого получения погоды в них:'
+                                           '\n---Напиши /city'
+                                           '\nИли удалить уже добавленные:'
+                                           '\n---Напиши /delcity'
+                                           '\nЕсли захочешь увидеть это сообщение еще раз:'
+                                           '\n---Напиши /start или /help'
+                                           '\nВведи что-нибудь')
+
+@bot.message_handler(commands=['city']) #будет выполняться при вводе /city
+def start(message):
+    bot.send_message(message.from_user.id, 'Эта команда позволяет добавить город для дальнейшего быстрого получения погоды в нем\nВведи желаемый город')
+    bot.register_next_step_handler(message, get_town)
+
+@bot.message_handler(commands=['delcity']) #будет выполняться при вводе /delcity
+def start(message):
+    if message.from_user.id in city:
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        for i in city[message.from_user.id]:
+            markup.add(i)
+        bot.send_message(message.from_user.id, 'Эта команда позволяет удалить город из быстрого получения погоды'
+                                               '\nВыберите город, который хотите удалить',reply_markup=markup)
+        bot.register_next_step_handler(message, del_town)
+    else:
+        bot.send_message(message.from_user.id,'У вас нет добавленных городов')
+
+city={}
+
+def del_town(message):
+    id=message.from_user.id
+    if len(city[id])==1:
+        del city[id]
+    else:
+        city[id]=[v for v in city[id] if v!=message.text]
+    bot.reply_to(message,"Город успешно удален")
+
+def get_town(message):
+    id=message.from_user.id
+    try:
+        wo = owm.weather_at_place(message.text).get_weather()
+        if id in city:
+            city[id].append(message.text)
+        else:
+            city[id] = [message.text]
+        bot.reply_to(message, 'Город успешно добавлен')
+    except:
+        bot.reply_to(message,'Такого города не существует')
 
 @bot.message_handler(content_types=['text']) #функция выполняющаяся при каждом вводе текстового сообщения
 def content(message):
@@ -72,18 +119,23 @@ def content(message):
 
 def get_city(message): # приглашает ввести желаемый город
     if message.text=='Узнать погоду':
-        bot.send_message(message.from_user.id, "Введи город, для которого хочешь узнать погоду")
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        if message.from_user.id in city:
+            print(city[message.from_user.id])
+            for i in city[message.from_user.id]:
+                markup.add(i)
+        bot.send_message(message.from_user.id, "Введи город, для которого хочешь узнать погоду",reply_markup=markup)
         bot.register_next_step_handler(message, get_temperature)
 
 def get_temperature(message): #выводит температуру
-    city=message.text
+    city1=message.text
     try:
-        wo = owm.weather_at_place(city).get_weather()
+        wo = owm.weather_at_place(city1).get_weather()
         temp = wo.get_temperature('celsius')
         temp_now=temp['temp']
         temp_min=temp['temp_min']
         temp_max=temp['temp_max']
-        bot.send_message(message.from_user.id,"В городе " +str(city)+ " сейчас " + str(temp_now) +
+        bot.send_message(message.from_user.id,"В городе " +str(city1)+ " сейчас " + str(temp_now) +
                          "°С\nМин: "+str(temp_min)+'°С     '+'Макс: '+str(temp_max)+'°С')
     except:
         bot.send_message(message.from_user.id,"Такого города не существует")
